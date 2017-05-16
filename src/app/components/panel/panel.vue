a6019385316dd595a097bfbb05a93c95e9174bf0
Gonçalo Abreu <gabreu@blissapplications.com>
2017-05-16 11:39:40 +0100

    component, layout and mixins global registration

--- src/app/components/panel/panel.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    Panel component is ready for use!
-  </div>
-</template>
-<script src="./panel.js" lang="babel"></script>
-<style src="./panel.scss" lang="scss" scoped></style>
